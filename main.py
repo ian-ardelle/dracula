c862908f2e0d3d6aab4f34c6c2cea8eff39c5583
Ian <jerseyjackal@gmail.com>
2020-06-26 13:50:57 -0400

    Shotgun updates result in big brain bugs

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Vampire: The Masquerade"))
     print(f"Successfully logged in and booted!")
     for guild in db.get_guild_list():
-        await bot.get_channel(db.get_guild_info(guild[0]).get("announcements_channel")).send("Drac's back, baby!")
+        await bot.get_channel(db.get_guild_info(guild[0]).get("announcements_chan")).send("Drac's back, baby!")
 
 bot.run(DISCORD_API_KEY, bot=True, reconnect=True)
