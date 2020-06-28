e23f3d74bf6963a471f84d2e0b9169b375c43e8b
Ian <jerseyjackal@gmail.com>
2020-06-28 00:10:15 -0400

    Shotgun updates result in big brain bugs

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -29,7 +29,7 @@ class Time(commands.Cog):
             last_date = guild.get("last_date")
             if cur_date_dt != last_date:
                 cur_date = time.ic_date(guild_id)
-                channel = self.bot.get_channel(guild.get("date_chan"))
+                channel = self.bot.get_guild(guild_id).get_channel(guild.get("date_chan"))
                 await channel.send("The date is now: " + cur_date + ". Your hunger grows.")
                 db.execute("UPDATE Config SET last_date = %s WHERE guild_id = %s", (cur_date_dt.strftime("%Y:%m:%d"), guild_id))
                 player_list = db.get_all_players(guild_id)
