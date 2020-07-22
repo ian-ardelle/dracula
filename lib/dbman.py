da589549b6731efa487449d1f6cf7f806e7a00b5
Ian <jerseyjackal@gmail.com>
2020-07-22 12:56:51 -0400

    drac not parsing last_dates correctly after updating modules

--- lib/dbman.py
+++ lib/dbman.py
@@ -41,8 +41,10 @@ def get_guild_list():
 def get_guild_info(guild_id):
     c.execute("SELECT * FROM Config WHERE guild_id = %s", (guild_id,))
     guild_info = c.fetchone()
-    irl_start = datetime(int(guild_info[4][0:4]), int(guild_info[4][5:7]), int(guild_info[4][8:10]), int(guild_info[4][11:13]), int(guild_info[4][14:16]))
-    ic_start = datetime(int(guild_info[5][0:4]), int(guild_info[5][5:7]), int(guild_info[5][8:10]), int(guild_info[5][11:13]), int(guild_info[5][14:16]))
+    irl_raw = guild_info[4].decode('utf-8')
+    ic_raw = guild_info[5].decode('utf-8')
+    irl_start = datetime(int(irl_raw[0:4]), int(irl_raw[5:7]), int(irl_raw[8:10]), int(irl_raw[11:13]), int(irl_raw[14:16]))
+    ic_start = datetime(int(ic_raw[0:4]), int(ic_raw[5:7]), int(ic_raw[8:10]), int(ic_raw[11:13]), int(ic_raw[14:16]))
     return dict(id=guild_info[0], guild_id=guild_info[1], prefix=guild_info[2], date_coefficient=guild_info[3], ic_start=ic_start, irl_start=irl_start,
                 tz=timezone(guild_info[6]), date_chan=guild_info[7], feeding_chan=guild_info[8],
                 st_alerts_chan=guild_info[9], announcements_chan=guild_info[10], st_id=guild_info[11],
@@ -56,7 +58,8 @@ def get_player_info(guild_id, player_id):
     c.execute("SELECT * FROM Characters WHERE guild_id = %s AND player_id = %s", (g_id, player_id))
     player = c.fetchone()
     if player[7] != ' ':
-        upkeep_dt = datetime(int(player[7][0:4]), int(player[7][5:7]), int(player[7][8:10]), int(player[7][11:13]), int(player[7][14:16]), int(player[7][17:19]))
+        raw = player[7].decode('utf-8')
+        upkeep_dt = datetime(int(raw[0:4]), int(raw[5:7]), int(raw[8:10]), int(raw[11:13]), int(raw[14:16]), int(raw[17:19]))
     else:
         upkeep_dt = ' '
     formatted_player = dict(id=player[0], player_id=player[1], bp_max=player[2], bp=player[3], wp_max=player[4], wp=player[5], upkeep=player[6], upkeep_date=upkeep_dt, agg_dmg=player[8], alert_flag=player[9], guild_id=player[10])
@@ -70,8 +73,9 @@ def get_all_players(guild_id):
     formatted_player_list = []
     for player in player_list:
         if player[7] != ' ':
-            upkeep_dt = datetime(int(player[7][0:4]), int(player[7][5:7]), int(player[7][8:10]), int(player[7][11:13]),
-                                 int(player[7][14:16]), int(player[7][17:19]))
+            raw = player[7].decode('utf-8')
+            upkeep_dt = datetime(int(raw[0:4]), int(raw[5:7]), int(raw[8:10]), int(raw[11:13]),
+                                 int(raw[14:16]), int(raw[17:19]))
         else:
             upkeep_dt = ' '
         formatted_player = dict(id=player[0], player_id=player[1], bp_max=player[2], bp=player[3], wp_max=player[4], wp=player[5], upkeep=player[6], upkeep_date=upkeep_dt, agg_dmg=player[8], alert_flag=player[9], guild_id=player[10])
