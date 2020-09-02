bf40e2c9822f88c6d234994345585f9e86a7965d
Ian <jerseyjackal@gmail.com>
2020-09-02 14:10:20 -0400

    testing functions

--- lib/dbman.py
+++ lib/dbman.py
@@ -118,16 +118,19 @@ def get_all_players(guild_id):
     player_list = c.fetchall()
     formatted_player_list = []
     for player in player_list:
-        raw = player[7].decode("utf-8")
-        if raw != " ":
-            upkeep_dt = datetime(
-                int(raw[0:4]),
-                int(raw[5:7]),
-                int(raw[8:10]),
-                int(raw[11:13]),
-                int(raw[14:16]),
-                int(raw[17:19]),
-            )
+        if player[7]:
+            raw = player[7].decode("utf-8")
+            if raw != " ":
+                upkeep_dt = datetime(
+                    int(raw[0:4]),
+                    int(raw[5:7]),
+                    int(raw[8:10]),
+                    int(raw[11:13]),
+                    int(raw[14:16]),
+                    int(raw[17:19]),
+                )
+            else:
+                upkeep_dt = " "
         else:
             upkeep_dt = " "
         formatted_player = dict(
