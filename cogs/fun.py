5d173f2416023b8af1c3d33f4bc9103fbcf5bdaa
Ian Ardelle <ian.ardelle@outlook.com>
2020-09-27 14:21:42 -0400

    removed moderation commands, fixed director creation checks for scrape

--- cogs/fun.py
+++ cogs/fun.py
@@ -58,56 +58,6 @@ class Fun(commands.Cog):
                     elif white == 2:
                         await ctx.send(f"Starting game for {p1} and {p2}, {p2} to start as white.")
 
-    ##############
-    # Moderation #
-    ##############
-    @commands.command()
-    async def jail(self, ctx, member_id):
-        member = ctx.guild.get_member(int(member_id))
-        for role in member.roles:
-            if role == ctx.guild.get_role(756212060441804811):
-                await ctx.send("This user is already jailed.")
-            else:
-                role_list = []
-                for role2 in member.roles:
-                    role_list.append(role2.id)
-                jail_log = open(f"jail_log_{ctx.guild.id}.bin", "rb")
-                try:
-                    old_log = pickle.load(jail_log)
-                except EOFError:
-                    old_log = {}
-                old_log[f"{member.id}"] = role_list
-                await member.edit(roles=[ctx.guild.get_role(756212060441804811)])
-                jail_log.close()
-                jail_log = open(f"jail_log_{ctx.guild.id}.bin", "wb")
-                pickle.dump(old_log, jail_log)
-                jail_log.close()
-
-    @commands.command()
-    async def pardon(self, ctx, member_id):
-        member = ctx.guild.get_member(int(member_id))
-        for role in member.roles:
-            if role != ctx.guild.get_role(756212060441804811):
-                await ctx.send("This user is not jailed.")
-            else:
-                jail_log = open(f"jail_log_{ctx.guild.id}.bin", "rb")
-                old_log = pickle.load(jail_log)
-                old_roles = old_log[f"{member.id}"]
-                new_roles = []
-                for role2 in old_roles:
-                    new_roles.append(ctx.guild.get_role(role2))
-                print(new_roles)
-                await member.add_roles(*new_roles)
-                await member.remove_roles(756212060441804811)
-                old_log.pop(f"{member.id}")
-                jail_log.close()
-                jail_log = open(f"jail_log_{ctx.guild.id}.bin", "wb")
-                if old_log == {}:
-                    pickle.dump({}, jail_log)
-                else:
-                    pickle.dump(old_log, jail_log)
-                jail_log.close()
-
 
 def setup(bot):
     bot.add_cog(Fun(bot))
--- cogs/misc.py
+++ cogs/misc.py
@@ -60,17 +60,14 @@ class Misc(commands.Cog):
     async def scrape(self, ctx, cid):
         counter = 0
         guild_name = ctx.guild.name
+        working_dir = pathlib.Path.cwd() / "backups"
+        if not working_dir.exists():
+            working_dir.mkdir()
         guild_dir = pathlib.Path.cwd() / "backups" / guild_name
         if not guild_dir.exists():
             guild_dir.mkdir()
         chan_name = ctx.guild.get_channel(int(cid)).name
         await ctx.send("Archiving channel...")
-        working_dir = pathlib.Path.cwd() / "backups"
-        if not working_dir.exists():
-            working_dir.mkdir()
-        working_dir = pathlib.Path.cwd() / "backups" / guild_name
-        if not working_dir.exists():
-            working_dir.mkdir()
         working_dir = pathlib.Path.cwd() / "backups" / guild_name / chan_name
         if not working_dir.exists():
             working_dir.mkdir()
